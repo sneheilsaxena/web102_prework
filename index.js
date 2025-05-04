34e545101234e90e8c1687e50a01d24e27cf757d
Sneheil Saxena <sneheilsaxena@gmail.com>
2025-05-03 17:19:28 -0700

    Challenge 7

--- index.js
+++ index.js
@@ -175,7 +175,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, secondGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameEl = document.createElement("p");
+topGameEl.innerHTML = `${topGame.name}`;
+firstGameContainer.appendChild(topGameEl);
 
 // do the same for the runner up item
+const secondGameEl = document.createElement("p");
+secondGameEl.innerHTML = `${secondGame.name}`;
+secondGameContainer.appendChild(secondGameEl);
